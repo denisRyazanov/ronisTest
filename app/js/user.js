30aa8417d7798e91f1fbd65262f4f4c0bc4c3339
denisRyazanov <nppteploautomat2000@gmail.com>
2017-10-29 22:06:46 +0200

    main.ver.1.0

--- app/js/user.js
+++ app/js/user.js
@@ -1,31 +1,12 @@
 
 $( document ).ready(function() {
 	$('.single-item').slick({
-		dots: true,
+		dots: false,
 		arrows: true,
 		infinite: true,
 		speed: 300,
-		slidesToShow: 1
-	});
-});
-
-$(document).ready(function() {
-	$('.btnFirst').mouseover(function() {
-		$('.first').addClass('active');
-	});
-	$('.btnFirst').mouseout(function() {
-		$('.first').removeClass('active');
-	});
-	$('.btnSecond').mouseover(function() {
-		$('.second').addClass('active');
-	});
-	$('.btnSecond').mouseout(function() {
-		$('.second').removeClass('active');
-	});
-	$('.btnThird').mouseover(function() {
-		$('.third').addClass('active');
-	});
-	$('.btnThird').mouseout(function() {
-		$('.third').removeClass('active');
+		slidesToShow: 1,
+		autoplay: true,
+		
 	});
 });
